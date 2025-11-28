7900ce8b255e667224087251e651f463a82c7910
mohamedElbalky <mohammed4elbalky@gmail.com>
2025-11-28 16:10:17 +0200

    refactor: make email field required in register and add Documnet model to admin panal

--- api/serializers.py
+++ api/serializers.py
@@ -8,10 +8,16 @@ from .models import Document
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
+    email = serializers.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password', 'password2')
+    
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already exists.")
+        return value
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
